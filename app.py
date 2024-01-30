ac34488892ea50fd6f4134fb846d9a8a5fc529a5
utsav <sav.acharya22@gmail.com>
2024-01-30 22:22:39 +1100

    data schema

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 
 
-from flask import Flask
+from flask import Flask, request
 
 from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql+psycopg2://new_database_dev:123456@localhost:5432/new_database"
 
 db = SQLAlchemy(app)
+ma= Marshmallow(app)
 
 # create table
 
@@ -21,7 +23,12 @@ class Product(db.Model):
     stock = db.Column(db.Integer)
     description = db.Column(db.String(1000))
 
+class ProductSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'name', 'price','stock', 'description')
 
+products_schema = ProductSchema(many=True)
+product_schema = ProductSchema(many=False)
 
 # cli command
     
@@ -52,10 +59,38 @@ def drop_table():
 def get_products():
     stmt = db.select(Product)
     product_list = db.session.scalars(stmt)
-    return product_list
+    # convert to JSON
+
+    data = products_schema.dump(product_list)
+    return data
+
+@app.route('/products/<int:product_id>')
+def get_product(product_id):
+    stmt = db.select(Product).filter_by(id=product_id)
+    product = db.session.scalar(stmt)
+    if(product):
+
+        data = product_schema.dump(product)
+        return data
+    else:
+        return {'message': 'Product not found'}, 404
 
 
-
+@app.route('/products', methods= ['POST'])
+def create_product():
+    product_fields = request.get_json()
+    new_product = Product(
+        name = product_fields.get("name"),
+        price = product_fields.get("price"),
+        stock = product_fields.get("stock"),
+        description= product_fields.get("description")
+        
+    )
+    
+    db.session.add(new_product)
+    db.session.commit()
+    data = product_schema.dump(new_product)
+    return data, 201
 
 
 
